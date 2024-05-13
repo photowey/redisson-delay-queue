f5efcc14b3c54d65a851ff8d2ad879190e927360
photowey <photowey@gmail.com>
2024-05-13 23:28:13 +0800

    feat: Add: 1.redisson-delay-queue-spring-boot3-starter module.
    Signed-off-by: photowey <photowey@gmail.com>

---
+++ redisson-delay-queue-spring-boot3-starter/src/main/java/io/github/photowey/redisson/delay/queue/spring/boot/starter/config/RedissonDelayqueueStarterAutoConfigure.java
@@ -0,0 +1,35 @@
+/*
+ * Copyright © 2024 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.github.photowey.redisson.delay.queue.spring.boot.starter.config;
+
+import io.github.photowey.redisson.delay.queue.autoconfigure.config.RedissonDelayqueueAutoConfigure;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+
+/**
+ * {@code RedissonDelayqueueAutoConfigure}
+ *
+ * @author photowey
+ * @version 1.0.0
+ * @since 2024/05/13
+ */
+@Configuration
+@Import(value = {
+        RedissonDelayqueueAutoConfigure.class,
+})
+@ConditionalOnProperty(name = "spring.redis.redisson.delayqueue.enabled", havingValue = "true", matchIfMissing = false)
+public class RedissonDelayqueueStarterAutoConfigure extends AbstractRedissonDelayqueueStarterConfigure {}
